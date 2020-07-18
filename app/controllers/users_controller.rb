208f4384aa092a98f7a686217e3c81d7989f619f
Richard Ashby <richard.c.h.ashby@gmail.com>
2020-07-18 18:52:57 +0100

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You account information was successfully updated"
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
   def create
-    @user = User.new(users_params)
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
       redirect_to articles_path
@@ -15,7 +29,7 @@ class UsersController < ApplicationController
 
   private
 
-  def users_params
+  def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 end
